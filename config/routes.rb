1cbd75eda52586522f7a4fc2aaa62f61e35f5359
Dara Breen <dbreencode@gmail.com>
2014-02-09 20:36:02 +0000

    updated header

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Pinteresting::Application.routes.draw do
   devise_for :users
   root "pins#index"
   get "about" => "pages#about" # Creates about_path
-  get "features" => "pages#features" # Creates features_path
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
